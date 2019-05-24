afcd41646b9d2d29198b83007a59dd10c3e6e501
Marshall Offutt <marshalloffutt@gmail.com>
2019-05-24 14:30:18 -0500

    makes pizzas model, and migrates to db

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    """A class to resemble a pizza one might order"""
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        """Return a string representing name of pizza"""
+        return self.name
